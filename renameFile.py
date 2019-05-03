229ac8df2c9d2d82772e9298c1407f2bf4615b9a
KaShel06 <42469159+KaShel06@users.noreply.github.com>
2019-05-03 17:11:55 +0800

    Update renameFile.py

--- renameFile.py
+++ renameFile.py
@@ -1,18 +1,26 @@
 import os
 import re
-import random
+import time
+#import random
 
-def rename_file(path):
+def count_files(path):#文件夹数量
     os.chdir(path) #设为当前目录
     files = os.listdir(path) #获取目录下的文件
+    print(len(files)) #文件夹文件数量
+    return files
+
+def rename_files(files):
+    path = os.getcwd()
     for file in files:
-        filePath = path + '\\' + file
-        print(filePath)
-        os.rename(file, str(random.randint(20,300))+'.jpg')
-#    print(os.getcwd())
+        filePath = path.strip('\\') + '\\' + file #空盘符下会出现\\
+        print(filePath)#输出原文件名
+        #os.rename(file, file[-10:])
+        #os.rename(file, str(random.randint(20,300))+'.jpg')
 
 def main():
     path = input('输入路径:\n') #修改文件的路径
-    rename_file(path)
+    files = count_files(path)
+    rename_files(files)
+    #time.strftime('%Y%m%d',time.localtime(time.time())) 当前日期
 
 main()
